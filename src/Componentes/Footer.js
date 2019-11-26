4a026b837da35b30397e16fd6cd0b653f30bc352
RuteCMPereira <rutecmpereira99@gmail.com>
2019-11-26 18:03:55 +0000

    EDIT FOOTER

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -4,10 +4,21 @@ import '../Styles.css';
 
 function Footer() {
     return (
-        <div className={"row footer col-md-12"}>
+        <div className={"row footer justify-content-center"}>
 
-            <p className={"footertext"}>Entertainment Base 2019</p>
+            <div className={'col-2 '}>
+                <div className={'row justify-content-center' }>
+                <img className={'col-3 img-fluid p-2'} src={'IMGS/face_icon.png'}/>
+                <img className={'col-3 img-fluid p-2'} src={'IMGS/insta_icon.png'}/>
+                <img className={'col-3 img-fluid p-2'} src={'IMGS/twitter_icon.png'}/>
+                </div>
+                <div className={'row  text-center p-2'}>
+                    <p className={'col-12'}>Entertement Base 2018</p>
+                </div>
             </div>
+
+        </div>
+
     );
 }
 

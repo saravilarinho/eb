6030d77fe6b048b4d4fe1a62915ef58d2dcdbf7c
RuteCMPereira <rutecmpereira99@gmail.com>
2019-11-26 14:22:28 +0000

    ADD FOOTER AND MENU

---
+++ src/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Styles.css';
+
+
+function Menu() {
+    return (
+        <div>
+
+            <p>
+                Menu
+            </p>
+            </div>
+    );
+}
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -2,28 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Homepage from './Homepage';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Menu from './Menu';
+import Footer from './Footer'
 
-ReactDOM.render(<Router>
+ReactDOM.render(
+    <Router>
+        <Menu/>
+        <div>
+            <Switch>
+                <Route path="/homepage" component={Homepage}/>
+                <Route path="/movies"/>
+                <Route path="/series"/>
+                <Route path="/books"/>
+                <Route path="/music"/>
+                <Route path="/login"/>
+                <Route path="/registo"/>
+                <Route path="/favourites"/>
+                <Route component={Homepage}/>
+            </Switch>
 
-    <div>
-        <Switch>
-            <Route path="/homepage" component={Homepage}/>
-            <Route path="/movies"/>
-            <Route path="/series"/>
-            <Route path="/books"/>
-            <Route path="/music"/>
-            <Route path="/login"/>
-            <Route path="/registo"/>
-            <Route path="/favourites"/>
-
-
-            <Route component={Homepage}/>
-        </Switch>
-
-    </div>
-</Router>, document.getElementById('root'));
+        </div>
+    <Footer/>
+    </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

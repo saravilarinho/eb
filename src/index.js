f5ffb77fc6a5c00df9327841940a7ac17cc79d3a
sara <ssvilarinho@ua.pt>
2019-11-26 12:56:15 +0000

    BOOTSTRAP

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Homepage from './Homepage';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 ReactDOM.render(<Router>

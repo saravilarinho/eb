c2e027f6b183001deaf1b053292772bb6d7c8f4b
RuteCMPereira <rutecmpereira99@gmail.com>
2019-11-26 17:17:58 +0000

    EDIT BACKDROP

--- src/Componentes/Backdrop.js
+++ src/Componentes/Backdrop.js
@@ -4,13 +4,20 @@ import '../Styles.css';
 
 function Backdrop() {
     return (
-        <div className={'row'}>
 
-            <div className={'col-12'}>
 
-            <img src={'./src/Componentes/IMGS/Backdrop.png'} className={'img-fluid'}/>
+            <div className={'row Backdrop d-flex align-items-center'}>
+                <div className={'filler'}></div>
+                <h6 className={'col-12'}>FROM</h6>
+                <h1 className={'col-3 text-center'}>MOVIES</h1>
+                <h1 className={'col-3 text-center'}>SERIES</h1>
+                <h1 className={'col-3 text-center'}>BOOKS</h1>
+                <h1 className={'col-3 text-center'}>MUSIC</h1>
+                <h6 className={'col-12'}>WE HAVE EVERYTHING</h6>
+                <div className={'filler'}></div>
             </div>
-        </div>
+
+
     );
 }
 

9e63bab778edc1efc20258ad46d769b38c1e5bac
sara <ssvilarinho@ua.pt>
2019-11-26 12:35:17 +0000

    ADD ROUTING

---
+++ src/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Styles.css';
+
+function Homepage() {
+  return (
+    <div className="background">
+
+        <p>
+         Homepage
+        </p>
+
+
+    </div>
+  );
+}
+
+export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Homepage from './Homepage';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router>
+
+    <div>
+        <Switch>
+            <Route path="/homepage" component={Homepage}/>
+            <Route path="/movies"/>
+            <Route path="/series"/>
+            <Route path="/books"/>
+            <Route path="/music"/>
+            <Route path="/login"/>
+            <Route path="/registo"/>
+            <Route path="/favourites"/>
+
+
+            <Route component={Homepage}/>
+        </Switch>
+
+    </div>
+</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

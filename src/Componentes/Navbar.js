7d291ce461cef16331967e0860a07db0de9cb37f
sara <ssvilarinho@ua.pt>
2019-11-27 12:03:13 +0000

    NAVBAR + SEARCHBAR + LOGO

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Styles.css';
+import SearchBar from './SearchBar';
 import {Link} from 'react-router-dom'
 
 
@@ -7,15 +8,23 @@ function Navbar() {
     return (
         <div className={"row"}>
             <nav className={"navbar"}>
-                <Link to={"/Homepage"}>  <img src={"IMGS/Logo.png"} className={'img-fluid logo'}/>
-                </Link>
-                <div className={"links"}>
-                        <Link to={"/Movies"} >Movies</Link>
-                        <Link to={"/TvShows"}>TV Shows</Link>
-                        <Link to={"/Books"}>Books</Link>
-                        <Link to={"/Music"}>Music</Link>
-                        <Link to={"/LogIn"}>Log In/Sign In</Link>
+                <Link to={"/Homepage"} className={'logo_link'}>  <img src={"IMGS/Logo.png"} className={'img-fluid logo col-md-2'}/></Link>
+                <div>
+                    <span className="nav-item dropdown">
+                            <Link to={"/Movies"} className={"links nav-link dropdown-toggle col-md-2"}  >Movies</Link>
+                            <span className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a className="dropdown-item" href="#">Action</a>
+                                <a className="dropdown-item" href="#">Another action</a>
+                            </span>
+
+                    </span>
+                        <Link to={"/TvShows"} className={"links col-md-2"}>TV Shows</Link>
+                        <Link to={"/Books"} className={"links col-md-2"}>Books</Link>
+                        <Link to={"/Music"} className={"links col-md-2"}>Music</Link>
+                        <Link to={"/LogIn"} className={"links col-md-2"}>Log In/Sign In</Link>
                 </div>
+                <SearchBar className={'col-md-2'}/>
+
             </nav>
         </div>
     );

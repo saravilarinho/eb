ffd8a39734c9f93713f0322a82f51b9446d059fd
RuteCMPereira <rutecmpereira99@gmail.com>
2019-11-28 11:51:30 +0000

    ADD CAROUSSEL

--- src/Componentes/ListEntertaiment.js
+++ src/Componentes/ListEntertaiment.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import {Carousel} from 'react-responsive-carousel';
 import '../Styles.css';
 import img from "../IMGS/8b6b6ae81b05a817ac275d45fa09e18f10bf5c78_hq.jpg";
-
 
 function ListEntertaiment() {
     return (
 
-            <img className={'col-2 img-fluid p-1'} src={img}/>
+        <div className={'col-10 col-md-5 col-lg-3 listentertaiment p-2'}>
+            <img className={''} src={img}/>
+        </div>
     );
 }
 

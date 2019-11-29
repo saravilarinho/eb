200e47fc10fd56a752bf2fb2d1a1038a3ed757af
RuteCMPereira <rutecmpereira99@gmail.com>
2019-11-29 00:17:16 +0000

    adding props to components - start

--- src/Componentes/Line_20.js
+++ src/Componentes/Line_20.js
@@ -1,47 +1,65 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../Styles.css';
 import ListEntertaiment from '../Componentes/ListEntertaiment'
+import arrowfront from "../IMGS/chevron-front-solid.svg";
+import arrowback from "../IMGS/chevron-back-solid.svg";
 
 
-class Line20 extends Component {
+const Line20 = (props) =>  {
 
-    render() {
+        function sideScroll(element, direction, speed, distance, step) {
+            let scrollAmount = 0;
+            let slideTimer = setInterval(function () {
+                if (direction === 'left') {
+                    element.scrollLeft -= step;
+                } else {
+                    element.scrollLeft += step;
+                }
+                scrollAmount += step;
+                if (scrollAmount >= distance) {
+                    window.clearInterval(slideTimer);
+                }
+            }, speed);
+        }
 
         return (
 
 
             <section>
 
-                <h1 className={'titulos2 pb-5'}>Os Mais Populares...</h1>
+                <h1 className={'titulos2 pb-5'}>{props.titulo}</h1>
 
-                <ul className={'row justify-content-around mt-4'}>
+                <div className={'row justify-content-between controls w-100'}>
+                    <button className={'listentertaiment back'} id={"slideBack"} onClick={() => {
+                        let container = document.getElementById(props.titulo);
+                        sideScroll(container, 'left', 8, 400, 15);
+                    }}><img src={arrowback} className={'arrow'}/>
+                    </button>
 
 
-                    <li className="scroll-box col-12" id={'container'}>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
-                        <ListEntertaiment/>
+                    <button className={'listentertaiment front'} id={"slide"} onClick={() => {
+                        let container = document.getElementById(props.titulo);
+                        sideScroll(container, 'right', 8, 400, 15);
+                    }}><img src={arrowfront} className={'arrow'}/>
+                    </button>
+                </div>
+
+                <ul className={'row justify-content-around '}>
+
+
+                    <li className="scroll-box col-12" id={props.titulo}>
+
+                        {props.info.map((item) => <ListEntertaiment img={item.poster_path} text={item.overview} title={item.title}/>)}
+
                     </li>
+
 
                 </ul>
 
             </section>
 
         );
-    }
-}
+
+};
 
 export default Line20;
--- src/Componentes/ListEntertaiment.js
+++ src/Componentes/ListEntertaiment.js
@@ -5,28 +5,21 @@ import img from "../IMGS/8b6b6ae81b05a817ac275d45fa09e18f10bf5c78_hq.jpg";
 import eyeicon from '../IMGS/eye_icon.png'
 import hearticon from '../IMGS/heart-regular.svg'
 
-function ListEntertaiment() {
+const ListEntertaiment=(props)=>{
+
+    const path ='https://image.tmdb.org/t/p/w600_and_h900_bestv2/'+ props.img;
     return (
 
         <section className={'d-inline'}>
             <div className={'col-10 col-md-5 col-lg-3 listentertaiment'}>
-                <section className={'col-11'}>
-                    <img src={img} className={'w-100 d-block img-back'}/>
+                <section className={'col-11 h-100'}>
+                    <img src={path} className={'w-100 d-block img-back'}/>
                     <div className={'position-absolute above p-3'}>
-                        <h1> TITULO </h1>
-                        <p className={' text-adapt text-wrap'}>Lorem ipsum dolor sit amet, consectetur
-                            adipiscing elit. Suspendisse interdum
-                            turpis est. Aliquam
-                            mollis urna nec dui bibendum tempus. Praesent vel condimentum dolor. Pellentesque quis
-                            finibus
-                            tortor, non scelerisque magna.
-                            mollis urna nec dui bibendum tempus. Praesent vel condimentum dolor. Pellentesque quis
-                            finibus
-                            tortor, non scelerisque magna.
-
+                        <h1 className={'text-wrap'}> {props.title} </h1>
+                        <p className={' text-adapt text-wrap'}>{props.text}
                         </p>
-                        <div className={'row justify-content-around pt-5'}>
-                        <img className={'col-4 img-fluid pt-2'} src={eyeicon}/>
+                        <div className={'row justify-content-around pt-4'}>
+                        <img className={'col-4 img-fluid pt-2 pb-2'} src={eyeicon}/>
                         <img className={'col-3 img-fluid'} src={hearticon}/>
                         </div>
                     </div>

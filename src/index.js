e1196bfbcd579dcef65eabd556a0d3db94d54271
sara <ssvilarinho@ua.pt>
2019-12-02 11:54:17 +0000

    REDUX DONE

--- src/index.js
+++ src/index.js
@@ -12,27 +12,33 @@ import Music from "./Music";
 import Login from './Login'
 import SignIn from './SignIn'
 import List from  './List'
-import  Books from './Books'
+import Books from './Books'
+import RootReducer from './Reducers/RootReducer';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+const  store = createStore(RootReducer);
+
 
 ReactDOM.render(
-    <Router>
-        <Navbar/>
-            <Switch>
-                <Route path="/Homepage" component={Homepage}/>
-                <Route path="/Movies/:id_entertaiment" component={IndividualEntertaimentPage}/>
-                <Route path="/Movies" component={Movies}/>
-                <Route path="/Series/:id_entertaiment" component={IndividualEntertaimentPage}/>
-                <Route path="/List" component={List}/>
-                <Route path="/Series" component={Series}/>
-                <Route path="/Books" component={Books}/>
-                <Route path="/Music" component={Music}/>
-                <Route path="/Login" component={Login}/>
-                <Route path="/Registo" component={SignIn}/>
-                <Route path="/Favourites"/>
-                <Route component={Homepage}/>
-            </Switch>
-
-    <Footer/>
-    </Router>, document.getElementById('root'));
+    <Provider store={store}>
+        <Router>
+            <Navbar/>
+                <Switch>
+                    <Route path="/Homepage" component={Homepage}/>
+                    <Route path="/Movies/:id_entertaiment" component={IndividualEntertaimentPage}/>
+                    <Route path="/Movies" component={Movies}/>
+                    <Route path="/Series/:id_entertaiment" component={IndividualEntertaimentPage}/>
+                    <Route path="/List" component={List}/>
+                    <Route path="/Series" component={Series}/>
+                    <Route path="/Books" component={Books}/>
+                    <Route path="/Music" component={Music}/>
+                    <Route path="/Login" component={Login}/>
+                    <Route path="/Registo" component={SignIn}/>
+                    <Route path="/Favourites"/>
+                    <Route component={Homepage}/>
+                </Switch>
+                <Footer/>
+        </Router>
+    </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();

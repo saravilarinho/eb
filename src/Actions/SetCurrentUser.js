bfe518c3cb005cfed8a467f47d53214783dd4754
RuteCMPereira <rutecmpereira99@gmail.com>
2019-12-13 23:55:15 +0000

    Merge branch 'master' of https://github.com/saravilarinho/eb
    # Conflicts:
    #	src/Components/HorizontalList.js

--- src/Actions/SetCurrentUser.js
+++ src/Actions/SetCurrentUser.js
@@ -4,7 +4,7 @@ export const SetCurrentUser = (user) => {
 
     return (dispatch, getState) => {
 
-        dispatch({type: 'SIGN_IN', email: user.email})
+        dispatch({type: 'SIGN_IN', email: user.email , user:user})
     }
 
 };
